1b0eedf6a8f621fb4eac0c5bb931ae6e61ffb374
Zander Groenewald <zander.groenewald@gmail.com>
2022-09-29 09:36:55 +0200

    Can now add units to army list, and display it in Army page

---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorBattles.Client.Services
+{
+    public interface IBananaService
+    {
+        public event Action OnChange;
+        int Bananas { get; set; }
+        void EatBananas(int amount);
+        void AddBananas(int amount);
+
+    }
+}
